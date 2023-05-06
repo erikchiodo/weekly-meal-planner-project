5e81baa231519ce52895f899393681f6ba54f305
erikchiodo <erikchiodo@gmail.com>
2023-05-06 10:42:48 -0400

    Removing excess code from homeRoutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,10 +2,6 @@ const router = require("express").Router();
 const { User } = require("../models");
 const withAuth = require("../utils/auth");
 
-// router.get("/", async (req, res) => {
-//   res.render("login");
-// });
-
 router.get("/register", async (req, res) => {
   res.render("registration");
 });
